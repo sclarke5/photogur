4c29021a0c23e5d580ec42e083173240030ceae1
Sclarke5 <sclarke5555@gmail.com>
2018-04-22 16:44:04 -0400

    adds sessions controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
   resource :users, only: %i(new create)
 
   resource :sessions, only: %i(new create destroy)
