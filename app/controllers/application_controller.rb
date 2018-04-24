71a406d1472a9447e91b649c096891c6426ebb29
Sclarke5 <sclarke5555@gmail.com>
2018-04-24 15:20:12 -0400

    adds role-based authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+
 
   private
+
+  def ensure_logged_in
+    unless current_user
+      flash[:alert] = "Please log in"
+      redirect_to new_sessions_url
+    end
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
 
 
-  get 'sessions/new'
 
-  get 'sessions/create'
+  resource :users
 
-  resource :users, only: %i(new create)
-
-  resource :sessions, only: %i(new create destroy)
+  resource :sessions
 
 
   resources :pictures
